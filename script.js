653830d51b02b885a4c4791aa0111a1ee1aca7e4
Smith Gómez <smithsebas.1747@gmail.com>
2025-04-24 14:42:27 -0400

    Espero que esté completo :/

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Smith Gomez")
+document.getElementById("holamundo").textContent = "Adiós"
+document.getElementById("h3").style.color="orange"
+
+function nuevo_encabezado() {
+    document.getElementById("nuevo_encabezado").textContent = "Nuevo Encabezado"
+    document.getElementById("nuevo_encabezado").style.color= "brown"
+}
